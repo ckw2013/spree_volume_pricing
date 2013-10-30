7ecfffe7e8debe1795a195dd64a23deda2ffe0cb
ckw2013 <marketing.guru1@gmail.com>
2013-10-31 10:18:58 +1300

    Update line_item_decorator.rb

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -15,7 +15,7 @@ Spree::LineItem.class_eval do
       if changed? && changes.keys.include?('quantity')
         vprice = self.variant.volume_price(self.quantity)
 
-        if self.price.present? && vprice <= self.variant.price && current_user?
+        if self.price.present? && vprice <= self.variant.price
           self.price = vprice and return
         end
       end
