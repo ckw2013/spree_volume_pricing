51ba849e433256d414c45cc9f9bf3930438cb318
ckw2013 <marketing.guru1@gmail.com>
2013-10-31 13:11:05 +1300

    Update variant_decorator.rb

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -6,7 +6,7 @@ Spree::Variant.class_eval do
 
   # calculates the price based on quantity
   def volume_price(quantity)
-    if Spree::User.current.nil?
+    if User.current?
       return self.price
     else
       self.volume_prices.each do |volume_price|
